f7214e0bf7cab915873a2e7d02d67e837991e7bc
Pia Mendoza <lmendoza22@my.bcit.ca>
2024-09-25 14:14:29 -0700

    lesson 4 updates

---
+++ .history/mobile-app/app/(tabs)/sandwich_20240925140718.tsx
@@ -0,0 +1,27 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+
+export default function Page() {
+  return (
+        <View style={makeItPretty.sandwichPage}>
+            <Text style={makeItPretty.pageTitle}>This is a page about sandwiches 🥪</Text>
+            <Link href="/">This is my Index page</Link>
+            <Link href="/cheese">Cheese 🧀</Link>
+        </View>
+    )
+}
+
+const makeItPretty = StyleSheet.create({
+    sandwichPage: {
+        margin: 50,
+        borderColor: 'blue',
+        borderWidth: 2,
+        borderStyle: 'dashed'
+    },
+    pageTitle: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
