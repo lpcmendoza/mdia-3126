7df2dba4fc7bef4271b8307048e010fc952ea9ba
Pia Mendoza <lmendoza22@my.bcit.ca>
2024-09-25 13:17:43 -0700

    lesson 4 updates

---
+++ .history/mobile-app/app/(tabs)/cheese_20240925125314.tsx
@@ -0,0 +1,17 @@
+import { Text, View, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+
+
+export default function Page() {
+  return (
+        <View>
+            <Text className='border-red-700 border-2'>This is a page about cheese 🧀</Text>
+            <Link href="/sandwich">Sandwich 🥪</Link>
+            <Link href="/" asChild>
+                <Pressable>
+                    <Text>Index 🍓</Text>
+                </Pressable>
+            </Link>
+        </View>
+    )
+}
